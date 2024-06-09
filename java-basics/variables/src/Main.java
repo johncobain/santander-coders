e516a0abc03098e2cff57a16d874bc7ae4628e93
John <andreygdsn@gmail.com>
2024-06-09 13:20:53 -0300

    variables

---
+++ java-basics/variables/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int a;
+        int b = 2;
+        a = 3;
+        int sum = a+b;
+        int sub = a-b;
+        int time = a*b;
+        float div = (float) a/b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(time);
+        System.out.println(div);
+
+    }
+}
