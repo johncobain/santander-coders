f6f754ff51638b853f3b161010d0cd9ae75f87f3
John <andreygdsn@gmail.com>
2024-06-14 00:32:04 -0300

    trees

---
+++ introducao-estrutura-de-dados/EstruturasDeDados/src/main/java/Main/FilaDeAtendimento.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.util.Random;
+
+import dataStructure.Queue;
+
+public class FilaDeAtendimento {
+    public static void main(String[] args) {
+        int n = 5;
+        var random = new Random();
+        int cliente = random.nextInt(101);
+        Queue queue = new Queue(cliente);
+        System.out.println("Chegou o cliente: "+cliente);
+        //simula a chegada dos clientes
+        for(int i = 2; i<=n; i++){
+            cliente = random.nextInt(101);  
+            System.out.println("Chegou o cliente: "+cliente);
+            queue.enqueue(cliente);
+        }
+        //atendimento
+        var node = queue.dequeue();
+        while(node!= null){
+            System.out.println("Atendido o cliente: "+node.getValue());
+            node = queue.dequeue();
+        }
+    }
+}
